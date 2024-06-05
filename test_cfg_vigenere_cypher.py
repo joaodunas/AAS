d437524bd2b229eecfd46dbdaa72e709cc18af40
joao-a-g-alves <joaotonito.alves@gmail.com>
2024-06-05 20:50:58 +0100

    testes_cfg_vigenere

--- test_cfg_vigenere_cypher.py
+++ test_cfg_vigenere_cypher.py
@@ -1,44 +1,61 @@
-import pytest
+import unittest
 from vigenere_cipher import encrypt_message, decrypt_message
 
-def test_P1():
-    assert encrypt_message("RandomKey", "") == ""
+class TestVigenereCipher(unittest.TestCase):
+    def black_box_test(self):
+        self.assertEqualEqual(1, 1)
+    
+    #Empty message---------------------------------------------------------------------------------#
+    def test_P1(self):
+        self.assertEqual(encrypt_message("RandomKey", ""), "")
 
-def test_P2_1():
-    assert encrypt_message("RandomKey", "*") == "*"
+    #Message with just one character outside the alphabet------------------------------------------#
+    def test_P2_1(self):
+        self.assertEqual(encrypt_message("RandomKey", "*"), "*")
 
-def test_P2_2():
-    assert encrypt_message("RandomKey", "1234567890") == "1234567890"
+    #Message with various characters outside the alphabet------------------------------------------#
+    def test_P2_2(self):
+        self.assertEqual(encrypt_message("RandomKey", "1234567890"), "1234567890")
     
-def test_P2_3():
-    assert encrypt_message("RandomKey", "124353465* \"?*Ç!^^~~") == "124353465* \"?*Ç!^^~~"
+    #Message with even more characters outside the alphabet----------------------------------------#
+    def test_P2_3(self):
+        self.assertEqual(encrypt_message("RandomKey", "124353465* \"?*Ç!^^~~"), "124353465* \"?*Ç!^^~~")
     
-def test_P3_1():
-    assert encrypt_message("RandomKey", "A") == "R"
+    #Message with only one character inside the alphabet in uppercase in encrypt mode--------------#
+    def test_P3_1(self):
+        self.assertEqual(encrypt_message("RandomKey", "A"), "R")
     
-def test_P3_2():
-    assert encrypt_message("RandomKey", "AAAAAAAAA") == "RANDOMKEY"
+    #Message with various characters inside the alphabet in uppercase in encrypt mode--------------#
+    def test_P3_2(self):
+        self.assertEqual(encrypt_message("RandomKey", "AAAAAAAAA"), "RANDOMKEY")
     
-def test_P3_3():
-    assert encrypt_message("RandomKey", "LAJNSDOUASNJLDNASUDBNBUEIQNLAJSNDKAS") == "CAWQGPYYYJNWORZKWSUBAEIQSULCAWVBPUEQ"
+    #Message with even more characters inside the alphabet in encrypt mode-------------------------#
+    def test_P3_3(self):
+        self.assertEqual(encrypt_message("RandomKey", "LAJNSDOUASNJLDNASUDBNBUEIQNLAJSNDKAS"), "CAWQGPYYYJNWORZKWSUBAEIQSULCAWVBPUEQ")
     
-def test_P4_1():
-    assert decrypt_message("RandomKey", "r") == "a"
+    #Message with only one character inside the alphabet in lowercase in decrypt mode--------------#
+    def test_P4_1(self):
+        self.assertEqual(decrypt_message("RandomKey", "r"), "a")
     
-def test_P4_2():
-    assert decrypt_message("RandomKey", "zzzzzzzzz") == "izmwlnpvb"
+    #Message with various characters inside the alphabet in lowercase in decrypt mode--------------#
+    def test_P4_2(self):
+        self.assertEqual(decrypt_message("RandomKey", "zzzzzzzzz"), "izmwlnpvb")
     
-def test_P4_3():
-    assert decrypt_message("RandomKey", "rdaskndnqouwndalsdn") == "adnpwbtjsxujkpobofw"
+    #Message with even more characters inside the alphabet in lowercase in decrypt mode------------#
+    def test_P4_3(self):
+        self.assertEqual(decrypt_message("RandomKey", "rdaskndnqouwndalsdn"), "adnpwbtjsxujkpobofw")
     
-def test_P5_1():
-    assert encrypt_message("RandomKey", "A*") == "R*"
+    #Message with 2 characters, combining one from alphabet with one outside-----------------------#
+    def test_P5_1(self):
+        self.assertEqual(encrypt_message("RandomKey", "A*"), "R*")
     
-def test_P5_2():
-    assert encrypt_message("RandomKey", "A112LKDASMD") == "R112LXGOEWH"
+    #Message combining some characters from alphabet with some outside-----------------------------#
+    def test_P5_2(self):
+        self.assertEqual(encrypt_message("RandomKey", "A112LKDASMD"), "R112LXGOEWH")
     
-def test_P5_3():
-    assert encrypt_message("RandomKey", "A?#$%&/(AKSNDA*`ª^ª  ASD AB A134)") == "R?#$%&/(AXVBPK*`ª^ª  EQU AO D134)"
+    #Message combining even more characters from alphabet with some outside------------------------#
+    def test_P5_3(self):
+        self.assertEqual(encrypt_message("RandomKey", "A?#$%&/(AKSNDA*`ª^ª  ASD AB A134)"), "R?#$%&/(AXVBPK*`ª^ª  EQU AO D134)")
     
 if __name__ == "__main__":
-    pytest.main()
+    unittest.main()
